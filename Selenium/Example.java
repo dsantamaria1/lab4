3af3612fe8502542fac5289bf74f675996606e28
Santa Maria <drs2486@lib-pclem025.lib.utexas.edu>
2015-11-20 12:42:16 -0600

    added farenheit conversion 0 - 212

--- Selenium/Example.java
+++ Selenium/Example.java
@@ -9,32 +9,6 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class Example extends TestCase  {
 
-    public void testValidLogin(){
-        WebDriver driver = new HtmlUnitDriver();
-  	//get the website
-        driver.get("http://apt-public.appspot.com/testing-lab-login.html");
-        
-     	// Find the text user name and password elements by their name
-        WebElement userName = driver.findElement(By.name("userId"));
-        WebElement password = driver.findElement(By.name("userPassword"));
- 
-       //clear elements
-	userName.clear();
-	password.clear();
-
- 	userName.sendKeys("andy");
-	password.sendKeys("apple");
-        
-	// Now submit the form. WebDriver will find the form for us from the element
-        password.submit();
-
-        // Check the title of the page
-        assertEquals("Online temperature conversion calculator", driver.getTitle());
-
-        driver.quit();
-    }
-   
-
     public void testInvalidLogin(){
 
 	ArrayList<String> testUserNames = new ArrayList<String>();
@@ -45,8 +19,8 @@ public class Example extends TestCase  {
 	testUserNames.add("bo b");
 	testUserNames.add("bob");
 
+  	testPassWords.add("Bathtub");
   	testPassWords.add("bath");
-  	testPassWords.add("bathtub");
   	testPassWords.add("bathtub");
   	testPassWords.add("bath tub");
 
@@ -58,15 +32,15 @@ public class Example extends TestCase  {
         WebElement userName;
         WebElement password;
  
-       //clear elements
-        for(int i = 0; i < testUserNames.size(); i++){
-          System.out.println("iteration is " + i);   
+        //for(int i = 0; i < testUserNames.size(); i++){
+        for(int i = 0; i < 1; i++){
+          //System.out.println("iteration is " + i);   
   	  //get the website
           driver.get("http://apt-public.appspot.com/testing-lab-login.html");
-          if(i == 0){
-            userName = driver.findElement(By.name("userId"));
-            password = driver.findElement(By.name("userPassword"));
-          }
+          userName = driver.findElement(By.name("userId"));
+          password = driver.findElement(By.name("userPassword"));
+          
+          //clear elements
 	  userName.clear();
 	  password.clear();
 
@@ -85,36 +59,50 @@ public class Example extends TestCase  {
 
 
     //focus on one user profile
-    public void testUserNameCaseSensitive(){} 
-    public void testPassWordCaseSensitive(){}
+    public void testUserNameCaseSensitive(){
+        WebDriver driver = new HtmlUnitDriver();
+  	//get the website
+        driver.get("http://apt-public.appspot.com/testing-lab-login.html");
+        
+     	// Find the text user name and password elements by their name
+        WebElement userName = driver.findElement(By.name("userId"));
+        WebElement password = driver.findElement(By.name("userPassword"));
+ 
+        //clear elements
+	userName.clear();
+	password.clear();
 
+ 	userName.sendKeys(" CharLey");
+	password.sendKeys(" china ");
+        
+	// Now submit the form. WebDriver will find the form for us from the element
+        password.submit();
 
+        // Check the title of the page
+        assertEquals("Online temperature conversion calculator", driver.getTitle());
 
+	WebElement tempInput = driver.findElement(By.name("farenheitTemperature"));
+	tempInput.clear();
+	tempInput.sendKeys("0.00");
+	
+	tempInput.submit();
+	WebElement tempResult = driver.findElement(By.tagName("h2"));
+        assertEquals("0.00 Farenheit = -17.78 Celsius", tempResult.getText());
+	
+	driver.get("http://apt-public.appspot.com/testing-lab-calculator.html");
+	WebElement tempInput2 = driver.findElement(By.name("farenheitTemperature"));
+	tempInput2.clear();
+	tempInput2.sendKeys("212.00");
+	
+	tempInput2.submit();
+	WebElement tempResult2 = driver.findElement(By.tagName("h2"));
+        assertEquals("212.00 Farenheit = 100 Celsius", tempResult2.getText());
+        driver.quit();
+    }
+ 
 
     public static void main(String[] args) {
         String[] testCaseName = { Example.class.getName() };
         junit.textui.TestRunner.main(testCaseName);
-
-//        // Create a new instance of the html unit driver
-//        // Notice that the remainder of the code relies on the interface, 
-//        // not the implementation.
-//        WebDriver driver = new HtmlUnitDriver();
-//
-//        // And now use this to visit Google
-//        driver.get("http://www.google.com");
-//
-//        // Find the text input element by its name
-//        WebElement element = driver.findElement(By.name("q"));
-//
-//        // Enter something to search for
-//        element.sendKeys("Cheese!");
-//
-//        // Now submit the form. WebDriver will find the form for us from the element
-//        element.submit();
-//
-//        // Check the title of the page
-//        System.out.println("Page title is: " + driver.getTitle());
-//
-//        driver.quit();
     }
 }
